e2a7fc2c4bbec8256259afe9061a2eb6ee86c3d5
Shivam Garg <gargshivam34has@gmail.com>
2025-05-01 12:24:49 +0530

    added tailwind attempt 1

--- src/App.tsx
+++ src/App.tsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className="card bg-gray-100 p-4 rounded-lg shadow-md">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
