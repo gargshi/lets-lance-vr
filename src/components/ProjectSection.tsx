6337283e0584c9634b012c8351345948fb34328c
Shivam Garg <gargshivam34has@gmail.com>
2025-05-20 22:43:53 +0530

    updated-ui-project-list-dashboard-v3

--- src/components/ProjectSection.tsx
+++ src/components/ProjectSection.tsx
@@ -130,11 +130,11 @@ const ProjectSection:React.FC<ProjectSectionProps> = ({className=""}) => {
 				<option value="2">2 Columns</option>
 				<option value="3">3 Columns</option>
 			</select>
-			<div className="flex justify-center mt-4">
+			
 			<button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600 transition duration-200" onClick={() => setModalOpen(true)}>
 				Create New Project
 			</button>
-		</div>
+			
 		</div>
 		<Modal isOpen={isModalOpen} onClose={() => setModalOpen(false)} title="Add Project">
             <form className="space-y-4" onSubmit={(e) => { e.preventDefault(); addProject(); }}>
@@ -182,7 +182,7 @@ const ProjectSection:React.FC<ProjectSectionProps> = ({className=""}) => {
 		</Modal>
 		<Paginator currentPage={currentPage} totalPages={totalPages} handlePrev={handlePrev} handleNext={handleNext} />
 		<>
-			<div className={`grid grid-cols-1 md:grid-cols-2 lg:grid-cols-${noOfCols} gap-4`}>
+			<div className={`grid grid-cols-1 md:grid-cols-${noOfCols} lg:grid-cols-${noOfCols} gap-4`}>
 				{currentProjects.map((project) => (
 					<ProjectCard 
 						className={className} 
@@ -272,32 +272,6 @@ const ProjectCard: React.FC<ProjectCardProps> = ({ id, title, postedBy, projectD
 				</div>								
 				<p className="text-xs text-gray-20">Project ID: <span className="font-medium text-gray-30">{id}</span></p>
 			</div>
-
-			
-		
-			{/* <tr key={id} className={`${className} project-card border-b border-gray-300 hidden lg:table-row`}>
-				<td className="border border-gray-300 px-4">{id}</td>
-				<td className="border border-gray-300 px-4">{title}</td>
-				<td className="border border-gray-300 px-4">{projectDescription}</td>
-				<td className="border border-gray-300 px-4">${budget}</td>
-				<td className="border border-gray-300 px-4">{postedBy}</td>
-				<td className="border border-gray-300 px-4 flex space-x-2">
-					<button type="button" title="View" className="bg-blue-500 text-white px-2 py-1 rounded-lg hover:bg-blue-600 transition duration-200 ml-2" onClick={() => { openModalProject(); }}>
-						<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-							<path strokeLinecap="round" strokeLinejoin="round" d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z" />
-							<path strokeLinecap="round" strokeLinejoin="round" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-						</svg>
-					</button>
-					{canDelete && (
-						<button type="button" title="Delete" className="bg-red-500 text-white px-2 py-1 rounded-lg hover:bg-red-600 transition duration-200" onClick={() => DeleteProject(id)}>
-							<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-								<path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-							</svg>
-						</button>
-					)}
-					
-				</td>
-			</tr> */}
 		</>					
 		
 	)
