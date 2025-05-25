3da0fd36434985e3371bd6deb28e24f17210dc1d
Shivam Garg <gargshivam34has@gmail.com>
2025-05-25 19:12:22 +0530

    expander-in-notification-v1

--- src/components/MessagesPaginated.tsx
+++ src/components/MessagesPaginated.tsx
@@ -94,46 +94,7 @@ const MessageList: React.FC<MessageListProps> = ({type = 'system'}) => {
 			<div>
 				<div className={`grid grid-cols-1 md:grid-cols-1 lg:grid-cols-1 gap-4`}>
 					{currentMessages.map((msg) => (						
-						<div
-							className={`rounded-lg border border-gray-500 p-4 md:p-2 shadow-sm transition duration-300 ${sevClrBdr[msg.severity]} hover:border-1 cursor-default`}
-							key={msg.id}
-							>
-							<div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2">
-								<div className="flex flex-col sm:flex-row sm:items-center gap-4">
-									<div className='flex justify-between'>
-										<p className="text-xs flex space-x-2">
-											<span className={`border-4 p-1 w-1 h-1 rounded-xl ${sevClr[msg.severity]}`}></span>
-											<span className="font-medium ">{msg.sender}</span>
-										</p>
-										<button
-											className="text-red-800 hover:text-red-700 text-xs font-semibold px-1 py-1 rounded border border-red-500 hover:border-red-200 transition sm:hidden"
-											onClick={() => handleDelete(msg.id, 'system')}
-											title="Dismiss"
-										>
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-												<path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-											</svg>
-										</button>
-									</div>
-									<div className="text-sm">
-										
-										<p>{msg.content}</p>
-									</div>
-								</div>
-								<div className="flex items-center gap-2">
-									<p className="text-xs ">{timeAgo(msg.received_at) || new Date(msg.received_at).toLocaleString()}</p>
-									<button
-											className={`text-red-500 hover:text-red-700 text-xs font-semibold px-1 py-1 rounded border border-red-500 hover:border-red-200 transition hidden sm:block`}
-											onClick={() => handleDelete(msg.id, 'system')}
-											title="Dismiss"
-										>
-											<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
-												<path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
-											</svg>
-									</button>
-								</div>
-							</div>
-						</div>						
+						<MessageBlock key={msg.id} msg={msg} handleDelete={handleDelete} sevClr={sevClr} sevClrBdr={sevClrBdr} timeAgo={timeAgo} />
 					))}
 				</div>				
 			</div>
@@ -161,5 +122,53 @@ const Paginator: React.FC<{ currentPage: number; totalPages: number; handlePrev:
 	);
 }
 
+const MessageBlock: React.FC<{ msg: Message; handleDelete: (id: number, type: string) => void; sevClr: Record<string, string>;sevClrBdr: Record<string, string>; timeAgo: (dateString: string) => string }> = ({ msg, handleDelete, sevClr, sevClrBdr, timeAgo }) => {
+	const [revealMsg, setRevealMsg] = useState(false); // State to track if the message should be revealed
+	return (
+			<>
+				<div 
+					className={`rounded-lg border border-gray-500 p-4 md:p-2 shadow-sm transition duration-300 ${sevClrBdr[msg.severity]} hover:border-1 cursor-default`}
+					key={msg.id}
+					>
+					<div className="flex flex-col sm:flex-row sm:justify-between sm:items-center gap-2" onClick={() => setRevealMsg(!revealMsg)}>
+						<div className="flex flex-col sm:flex-row sm:items-center gap-4">
+							<div className='flex justify-between'>
+								<p className="text-xs flex space-x-2">
+									<span className={`border-4 p-1 w-1 h-1 rounded-xl ${sevClr[msg.severity]}`}></span>
+									<span className="font-medium ">{msg.sender}</span>
+								</p>
+								<button
+									className="text-red-800 hover:text-red-700 text-xs font-semibold px-1 py-1 rounded border border-red-500 hover:border-red-200 transition sm:hidden"
+									onClick={() => handleDelete(msg.id, 'system')}
+									title="Dismiss"
+								>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+										<path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+									</svg>
+								</button>
+							</div>
+							<div className="flex flex-col">
+								<div className={`text-xs relative`}>
+									<p>{(msg.content.length > 40 && !revealMsg) ? msg.content.slice(0, 40) + '...' : msg.content}</p>
+								</div>								
+							</div>
+						</div>
+						<div className="flex items-center gap-2">
+							<p className="text-xs ">{timeAgo(msg.received_at) || new Date(msg.received_at).toLocaleString()}</p>
+							<button
+									className={`text-red-500 hover:text-red-700 text-xs font-semibold px-1 py-1 rounded border border-red-500 hover:border-red-200 transition hidden sm:block`}
+									onClick={() => handleDelete(msg.id, 'system')}
+									title="Dismiss"
+								>
+									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-5">
+										<path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
+									</svg>
+							</button>
+						</div>
+					</div>
+				</div>
+			</>
+	)
+};
 
 export default MessageList;
